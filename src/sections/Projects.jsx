27bfec8a76f32bcbcbcf1d3e8d721c7cdebae5b8
Anuradha Herath <herathanuradha03@gmail.com>
2026-05-05 15:26:17 +0530

    fix view all project button bug

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -136,8 +136,8 @@ export const Projects = () => {
         </div>
 
         {/* View All CTA */}
-        <div className="text-center mt-12 animate-fade-in animation-delay-500">
-          <AnimatedBorderButton>
+        <div className="text-center mt-12 animate-fade-in animation-delay-500" onClick={() => window.open('https://github.com/Anuradha00Herath', '_blank')}>
+          <AnimatedBorderButton >
             View All Projects
             <ArrowUpRight className="w-5 h-5" />
           </AnimatedBorderButton>
